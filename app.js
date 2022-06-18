1b4661274c35f97b635ff54671f62924f3d36d42
Alexander Abanobi <alexabanobi.com>
2022-06-18 11:20:52 -0400

    working on the about page

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const methodOverride = require('method-override');
 //const Campground = require('./models/campground') // here i am requiring a certain file (class) called campground.js in the models directory
     // that campground file exports a mongoose schema class. more or less like returns that mongoose schema class
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { // create and connect to this database
+mongoose.connect('mongodb://localhost:27017/zankli-pp', { // create and connect to this database
     useNewUrlParser: true,
     useUnifiedTopology: true
 }); // here i am creating a database called yelp-camp and i am setting some mongoose options to ease operation and avoid errors
@@ -33,11 +33,18 @@ db.once('open', () => {
 const app = express(); // create the express app by just calling express()
 app.use(express.urlencoded({ encoded: true })); //very important line that tells it to parse the req.body  for a post request so it can be used 
 app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
 
 
 app.set('view engine', 'ejs'); // select ejs as the view engine because with ejs you can have dynamic content in your views
 app.set('views', path.join(__dirname, 'views')); // here tell the app that the views are in a directory that is the present directory appended with views.
 // this means that the views directory is just one level below the present working directory and it is named 'views'.
+
+
+app.get('/', (req, res) => {
+    res.render('about');
+})
+
 
 
 app.listen(3000, () => {
