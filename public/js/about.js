25feb0f0bb76dd9634a55c9fc640bfc418b6908b
Alexander Abanobi <alexabanobi.com>
2022-06-19 15:44:03 -0400

    javascript added on the services

--- public/js/about.js
+++ public/js/about.js
@@ -1,4 +1,58 @@
 console.log('I am working');
 const servicesdropdown = document.querySelector('.servicesdropdown-inactive');
-servicesdropdown.style.display = 'block'
+const servicelinkhead = document.querySelectorAll('.links')[2];
 
+// servicesdropdown.addEventListener('mouseover', (event) => {
+//   console.log(event);
+// })
+
+
+// servicesdropdown.addEventListener('mouseenter', e => {
+//    console.log('mouse has entered')
+//   });
+  
+// servicesdropdown.addEventListener('mouseleave', e => {
+//     console.log('Mouse has left')
+// });
+
+
+servicelinkhead.addEventListener('mouseenter', e => {
+    console.log('mouse has entered')
+    servicesdropdown.classList.remove('servicesdropdown-inactive')
+    servicesdropdown.classList.add('servicesdropdown-active')
+    console.dir(servicesdropdown)
+});
+
+servicelinkhead.addEventListener('mouseleave', e => {
+    console.log('Mouse has left')
+    servicesdropdown.classList.remove('servicesdropdown-active')
+    servicesdropdown.classList.add('servicesdropdown-inactive')
+
+});
+
+
+servicesdropdown.addEventListener('mouseenter', e => {
+    console.log('mouse has entered')
+    servicesdropdown.classList.remove('servicesdropdown-inactive')
+    servicesdropdown.classList.add('servicesdropdown-active')
+    console.dir(servicesdropdown)
+});
+
+servicesdropdown.addEventListener('mouseleave', e => {
+    console.log('Mouse has left')
+    servicesdropdown.classList.remove('servicesdropdown-active')
+    servicesdropdown.classList.add('servicesdropdown-inactive')
+
+});
+
+// colorboxes.forEach((value, index1) => {
+
+//     value.addEventListener('click', (event) => {
+//         if (event.target.style.backgroundColor == "") {
+//             console.log(`Hi, I do not have any color asigned to me !!, and I am at positon ${index1}`);
+//         } else {
+//             console.log(`Hi... the name of this color is ${event.target.style.backgroundColor} and I am at position ${index1+1}`);
+//         }
+//     })
+
+// })
